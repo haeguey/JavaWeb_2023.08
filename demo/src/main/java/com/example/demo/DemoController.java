47685f2eac35f09c4d1f6dd77d4eeb030bd57fa7
Hae Y. Min <daily.ritual.gargle@gmail.com>
2023-08-09 12:42:55 +0900

    JavaWeb Spring 2023-08-09

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -41,4 +41,87 @@ public class DemoController {
 		model.addAttribute("pwd", pwd);
 		return "03.loginResult";
 	}
+	
+	@GetMapping("/getParam")
+	@ResponseBody
+	public String getParam(HttpServletRequest req) {
+		// System.out.println("getParam()");
+		String a_ = req.getParameter("a");
+		String b_ = req.getParameter("b");
+		String op = req.getParameter("op");
+		int a = Integer.parseInt(a_);
+		int b = Integer.parseInt(b_);
+		int result = 0;
+		String oper = "";
+		switch(op) {
+			case "add":
+				result = a + b; oper = "+";
+				break;
+			case "sub":
+				result = a - b; oper = "-";
+				break;
+			case "mul":
+				result = a * b; oper = "*";
+				break;
+			case "div":
+				result = (int) (a / b); oper = "/";
+				break;
+			default:
+				result = 0;
+		}
+		return "<h1>" + a + oper + b + " = " + result + "</h1>";
+	}
+	
+	@GetMapping("/calc")
+	public String calcForm() {
+		return "04.calcForm";
+	}
+	
+	@PostMapping("/calc")
+	public String calcProc(int a, int b, String op, Model model) {
+		int result = 0;
+		String oper = "";
+		switch(op) {
+			case "add":
+				result = a + b; oper = "+";
+				break;
+			case "sub":
+				result = a - b; oper = "-";
+				break;
+			case "mul":
+				result = a * b; oper = "*";
+				break;
+			case "div":
+				result = (int) (a / b); oper = "/";
+				break;
+			default:
+				result = 0;
+		}
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		model.addAttribute("oper", oper);
+		model.addAttribute("result", result);
+		
+		return "05.calcResult";
+	}
+	
+	@GetMapping("/write")
+	public String writeForm() {
+		return "06.writeForm";
+	}
+	
+	@PostMapping("/write")
+	public String writeProc(HttpServletRequest req, Model model) {
+		String title = req.getParameter("title");
+		String[] languages = req.getParameterValues("language");
+		String content = req.getParameter("content");
+		
+		String joinLanguages = (languages == null)?"":String.join(", ",  languages);
+		// If (languages == null) is true, enter "". If false, String.join(...) 
+		Board board = new Board(title, joinLanguages, content.replace("\n", "<br>"));
+		// System.out.println(board);
+		model.addAttribute("board", board);
+		
+		return "07.writeResult";
+	}
 }
